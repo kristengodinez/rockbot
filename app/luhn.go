0449424fcdc8c250c2de69b8427c2d22548b6798
kristengodinez <kristen.go@outlook.com>
2024-08-12 08:05:33 -0700

    create func for luhn algorithm

--- app/luhn.go
+++ app/luhn.go
@@ -1,10 +1,34 @@
 package main
 
-// Multiply each digit, alternating between 2 and 1
-// If product is >= 10, then add the two digits
-// Sum the results
-// If sum % 10 == 0, then it is valid
+import (
+	"unicode"
+)
 
 func Luhn(number string) bool {
+	var factor int = 2
+	var sum int = 0
+	var product int = 1
+	for _, ch := range number {
+		if unicode.IsDigit(ch) {
+			product = (int(ch) - '0') * factor
+
+			if product >= 10 {
+				sum = sum + 1 + product%10
+			} else {
+				sum = sum + product
+			}
+
+			if factor == 2 {
+				factor = 1
+			} else {
+				factor = 2
+			}
+		}
+	}
+
+	if sum%10 == 0 {
+		return true
+	}
+
 	return false
 }
